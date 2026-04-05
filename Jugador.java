db44020910ad70928627887197039b761a4286c4
LAPTOP-N2SMS2BQ\limpi <lgb0030@alu.medac.es>
2026-04-05 19:29:20 +0200

    Constructores de jugadores metidas

---
+++ Jugador.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+
+public class Jugador {
+    private String nombre;
+    private LocalDate fechaNacimiento;
+    private String posicion;
+    private boolean traspasoSolicitado;
+
+    private static int totalJugadores;
+    
+     /**
+      * contructor de la clase jugador. 
+      * El atributo traspaso solicitado siempre se inicializa a false . 
+      * 
+      * @param Nombre de la camiseta del jugador. 
+      * @param fechaNacimiento Feche de nacimiento del jugador. 
+      * @param posicion Posicion del jugador. 
+      */   
+public Jugador(String nombre, LocalDate fechaNacimiento, String posicion) {
+
+    this.nombre = nombre;
+    this.fechaNacimiento = fechaNacimiento;
+    this.posicion = posicion; 
+    this.traspasoSolicitado = false;
+    
+}
+
+}
