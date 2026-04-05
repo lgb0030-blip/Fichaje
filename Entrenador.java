a4c98e70bdcb1cc6178d659b7c7c0084e9b9db89
LAPTOP-N2SMS2BQ\limpi <lgb0030@alu.medac.es>
2026-04-05 20:21:07 +0200

    En entrenador geters y seters añadida

--- Entrenador.java
+++ Entrenador.java
@@ -1,4 +1,40 @@
 public class Entrenador {
+
     private String nombre;
     private String formacionPreferida;
+
+    private static int totalEntrenadores = 0;
+    totalEntrenadores++;
+
+/***
+ * Constructor de la clase entrenador.
+ * 
+ * @param nombre Nombre del entrenador.
+ * @param formacionPreferida Formacion preferida del entrenador.
+ */
+
+public Entrenador(String nombre, String formacionPreferida) {
+    this.nombre = nombre;
+    this.formacionPreferida = formacionPreferida;
+}
+
+public String getNombre() {
+    return nombre;
+}
+
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+
+public String getFormacionPreferida() {
+    return formacionPreferida;
+}
+
+public void setFormacionPreferida(String formacionPreferida) {
+    this.formacionPreferida = formacionPreferida;
+}
+
+public static int getTotalEntrenadores() {
+    return totalEntrenadores;
+}
 }
